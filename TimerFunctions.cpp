f1d61a0a122d3b3a218b1117973112a21fe60efd
Emmet Ocampo <emmet.ocampo@epanalytics.com>
2024-04-16 00:17:29 +0000

    Issue #159: Changed an assert around keys
    TimerFunctions.cpp:
        Changed an assert around how EntryExitKeys is checked. One set is used for
    all images. SO checking if the set is empty will always error out on the second
    image. Changed it to check if the key we are about to add is NOT in the set.

--- TimerFunctions.cpp
+++ TimerFunctions.cpp
@@ -443,11 +443,17 @@ extern "C"
         // user can turn them on/off
         std::set<uint64_t> keys;
         DynamicPoints->GetAllDynamicKeys(keys);
-        assert(EntryExitKeys.empty());
+        //assert(EntryExitKeys.empty());
         for (auto it = keys.begin(); it != keys.end(); it++) {
             uint64_t k = (*it);
             if (GET_TYPE(k) == PointType_functionEntry || 
               GET_TYPE(k) == PointType_functionExit) {
+
+                assert(EntryExitKeys.find(k) == EntryExitKeys.find(k));
+                /*if (EntryExitKeys.find(k) != EntryExitKeys.find(k)) {
+                    //fprintf(stderr, "\t key found again!!\n");
+                    ErrorExit("hmm, found duplicate key, exiting\n");
+                }*/
                 EntryExitKeys.insert(k);
             }
         }
@@ -467,17 +473,6 @@ extern "C"
 
         image_key_t iid = *key;
 
-        // Only print one file with data from all images
-        static bool finalized = false;
-        if (finalized)
-            return NULL;
-
-        finalized = true;
-
-        if (DynamicPoints != NULL) {
-            delete DynamicPoints;
-        }
-
         if (AllData == NULL){
             ErrorExit("data manager does not exist. no images were intialized",
               MetasimError_NoImage);
@@ -492,10 +487,20 @@ extern "C"
         }
 
         if (!timers->master){
-            printf("Image is not master, skipping\n");
+            return NULL;
+        } 
+
+        // Only print one file with data from all images
+        static bool finalized = false;
+        if (finalized) {
             return NULL;
         }
 
+        finalized = true;
+
+        if (DynamicPoints != NULL) {
+            delete DynamicPoints;
+        }
 
         uint64_t appTimeEnd = read_timestamp_counter();
         struct timeval tvEnd;
