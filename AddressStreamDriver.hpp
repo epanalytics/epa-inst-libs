c74d1e91b5c7b4d6b874b7481456fc52225f56f0
Allyson Cauble-Chantrenne <allysonc@epanalytics.com>
2024-06-05 19:41:09 +0000

    Entropy feature: Added the entropy tool to Address Stream Driver

--- AddressStreamDriver.hpp
+++ AddressStreamDriver.hpp
@@ -29,6 +29,7 @@ class MemoryStreamHandler;
 class SamplingMethod;
 class AddressRangeTool;
 class CacheSimulationTool;
+class EntropyRangeTool;
 class ReuseDistanceTool;
 class ScatterGatherLengthTool;
 class SpatialLocalityTool;
@@ -58,6 +59,7 @@ class AddressStreamDriver {
     // Are we running these tools?
     bool runAddressRange;
     bool runCacheSimulation;
+    bool runEntropyRange;
     bool runHardwarePrefetching;
     bool runReuseDistance;
     bool runScatterLength;
@@ -139,6 +141,7 @@ class AddressStreamDriver {
 
     bool IsAddressRange() { return runAddressRange; }
     bool IsCacheSimulation() { return runCacheSimulation; }
+    bool IsEntropyRange() { return runEntropyRange; }
     bool IsHardwarePrefetching() { return runHardwarePrefetching; }
     bool IsReuseDistance() { return runReuseDistance; }
     bool IsScatterLength() { return runScatterLength; }
